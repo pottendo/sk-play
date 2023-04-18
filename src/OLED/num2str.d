1797d05b2554d1fcb1c0564687437fd1fe69ebef
pottendo <pottendo@gmx.net>
2023-04-18 23:27:21 +0200

    Cleanup.

--- src/OLED/num2str.d
+++
@@ -1,7 +0,0 @@
-OLED/num2str.o OLED/num2str.d: OLED/num2str.cpp OLED/num2str.h \
- ../../circle-stdlib/install/aarch64-none-circle/include/stdint.h \
- ../../circle-stdlib/install/aarch64-none-circle/include/machine/_default_types.h \
- ../../circle-stdlib/install/aarch64-none-circle/include/sys/features.h \
- ../../circle-stdlib/install/aarch64-none-circle/include/_newlib_version.h \
- ../../circle-stdlib/install/aarch64-none-circle/include/sys/_intsup.h \
- ../../circle-stdlib/install/aarch64-none-circle/include/sys/_stdint.h
