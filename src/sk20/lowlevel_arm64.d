7140d5f987a4d226929b70b33bfb194d7141acdb
pottendo <pottendo@gmx.net>
2023-04-18 23:30:44 +0200

    Cleanup.

--- src/sk20/lowlevel_arm64.d
+++
@@ -1,5 +0,0 @@
-sk20/lowlevel_arm64.o sk20/lowlevel_arm64.d: sk20/lowlevel_arm64.cpp \
- sk20/lowlevel_arm64.h \
- ../../circle-stdlib/libs/circle/include/circle/types.h \
- ../../circle-stdlib/libs/circle/include/assert.h \
- ../../circle-stdlib/libs/circle/include/circle/macros.h
